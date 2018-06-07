bbdcfd32037ace550d4d4cecb94b63a4e665eea7
andiva-mira <andiva.mira@gmail.com>
2018-06-07 21:29:24 +0100

    favicon generator, url-loader, style changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
 	entry: ['babel-polyfill', './src/scripts/App.js'],
 	output: {
-		path: path.join(__dirname, '/docs'),
+		path: path.join(__dirname, '/docs/'),
 		filename: '[name].[hash].js'
 	},
 	
@@ -18,23 +19,38 @@ module.exports = {
 	},
 
 	optimization: {
-	minimizer: [
-	  new UglifyJsPlugin({
-		 uglifyOptions: {
-		  output: {
-			comments: false,
-			beautify: false,
-		  },
+		splitChunks: {
+			cacheGroups: {
+				styles: {
+					name: 'styles',
+					test: /\.css$/,
+					chunks: 'all',
+					enforce: true
+				}
+			}
+		},
 
-		}
-	  }), 
-	  new OptimizeCSSAssetsPlugin({
-		 assetNameRegExp: /\.css$/g,
-		 cssProcessor: require('cssnano'),
-		 cssProcessorOptions: { discardComments: { removeAll: true } },
-		 canPrint: true
-	  })
-	]
+		minimizer: [
+		new UglifyJsPlugin({
+			uglifyOptions: {
+				output: {
+					comments: false,
+					beautify: false
+				},
+
+				cache: true,
+				sourceMap: true
+
+
+			}
+		}),
+		new OptimizeCSSAssetsPlugin({
+			assetNameRegExp: /\.css$/g,
+			cssProcessor: require('cssnano'),
+			cssProcessorOptions: { discardComments: { removeAll: true } },
+			canPrint: true
+		})
+		]
 	},
 
 	plugins: [
@@ -44,8 +60,26 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({
 		  filename: 'styles.[hash].css',
-		  chunkFilename: "[id].css"
+		 // chunkFilename: "[id].css"
 		}),
+		new FaviconsWebpackPlugin({
+				logo: './src/news-icon.png',
+				prefix: 'favicon-[hash]/',
+				persistentCache: true,
+				inject: true,
+				icons: {
+					android: true,
+					appleIcon: true,
+					appleStartup: false,
+					coast: false,
+					favicons: true,
+					firefox: true,
+					opengraph: false,
+					twitter: false,
+					yandex: false,
+					windows: false
+				}
+		})
 	],
 
 	module: {
@@ -62,12 +96,21 @@ module.exports = {
 				use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
 			},
 			{
+			test: /\.(png|jpg|gif)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8192
+					}
+				}]
+			},
+			{
 				test: /\.svg/,
 				use: {
 					loader: 'svg-url-loader',
 					options: {}
 				}
-			}
+			},
 		]
 	}
 
